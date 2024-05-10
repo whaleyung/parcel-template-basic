abc875cd43aae1d0f4b9ba0db15dc130b6600f9a
whaleyung <whaleyung@gmail.com>
2024-05-10 10:53:30 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,24 @@
+// ESM
+// CommonJS
+
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// export {
+//     plugins: [
+//         autoprefixer
+//     ]
+// }
+// module.exports = {
+//     plugins: [
+//         autoprefixer
+//     ]
+// }
+
+
+//  **간소화
+module.exports = {
+    plugins: [
+        require('autoprefixer')
+    ]
+}
